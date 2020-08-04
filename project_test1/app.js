1eb13222c869c431dc5821126f752cd690241a72
Phi Tran <phitran@Phis-MBP-2.attlocal.net>
2020-08-03 19:08:38 -0500

    starting logic test

--- project_test1/app.js
+++ project_test1/app.js
@@ -1,4 +1,7 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn')
+const color = document.querySelector(".color")
 
-
-// testing 
+btn.addEventListener("click", function(){
+    
+})
