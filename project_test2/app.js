127a42ff0fdb429374dc44f6efa9f5c4995aff8b
ptran12 <phitran.722@gmail.com>
2020-08-07 12:44:41 -0500

    decrease button logic

---
+++ project_test2/app.js
@@ -0,0 +1,16 @@
+let count = 0;
+
+// selecting value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const styles = e.currentTarget.classList;
+        console.log(e.currentTarget.classList);
+        if (styles.contains("decrease")) {
+            count--;
+        }
+        value.textContent = count; 
+    });
+});
