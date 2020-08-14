1521636cdc766b41a3d5f46dea83b2447316be29
ptran12 <phitran.722@gmail.com>
2020-08-14 16:30:49 -0500

    fixed html due to error and worked on js btns

--- project_test3/app.js
+++ project_test3/app.js
@@ -39,19 +39,53 @@ const reviews = [
 ];
 
 // select items 
-const img = document.getElementById("person_img")
+const img = document.getElementById("person-img")
 const author = document.getElementById("author")
 const job = document.getElementById("job")
 const info = document.getElementById("info")
 
 const prevBtn = document.querySelector(".prev-btn")
 const nextBtn = document.querySelector(".next-btn")
-const randomvBtn = document.querySelector(".random-btn")
+const randomBtn = document.querySelector(".random-btn")
 
 // starting item 
 let currentItem = 0;
 
 //load intial item 
-window.addEventListener("DOMContentLoaded", function(){
-  console.log("test");
+window.addEventListener("DOMContentLoaded", function () {
+  showPerson(currentItem)
+}); 
+
+//show person based on item
+function showPerson(person){
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+//show next person
+nextBtn.addEventListener("click", function(){
+  currentItem++;
+  if(currentItem > reviews.length - 1){
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+})
+
+//show prev person
+prevBtn.addEventListener("click", function(){
+  currentItem--;
+  if(currentItem < 0){
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+})
+
+// show random person
+randomBtn.addEventListener("click", function(){
+  currentItem = Math.floor(Math.random() * reviews.length);
+  // console.log(currentItem)
+  showPerson(currentItem)
 })
