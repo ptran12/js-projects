6f8491869999d689e502c568186cd242da4bbd1a
ptran12 <phitran.722@gmail.com>
2020-08-18 15:42:52 -0500

    shorter version of toggle

--- project_test4/app.js
+++ project_test4/app.js
@@ -11,11 +11,13 @@ navToggle.addEventListener("click", function(){
     // console.log(links.classList);
     // console.log(links.classList.contains("random"));
     // console.log(links.classList.contains("links"));
-    if(links.classList.contains("show-links")){
-        // if class of show links is there, we are removing 
-        links.classList.remove("show-links")
-    } else {
-        links.classList.add("show-links")
-        // if class is not there aka it is false, we are showing the links
-    }
+    // if(links.classList.contains("show-links")){
+    //     // if class of show links is there, we are removing 
+    //     links.classList.remove("show-links")
+    // } else {
+    //     links.classList.add("show-links")
+    //     // if class is not there aka it is false, we are showing the links
+    // }
+    links.classList.toggle("show-links")
+    // shorter version
 })
