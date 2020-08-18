0b847e08e078770df94b8664a3398acb45c23800
ptran12 <phitran.722@gmail.com>
2020-08-18 15:40:35 -0500

    nav toggle

--- project_test4/app.js
+++ project_test4/app.js
@@ -3,3 +3,19 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle")
+const links = document.querySelector(".links")
+
+navToggle.addEventListener("click", function(){
+    // console.log(links.classList);
+    // console.log(links.classList.contains("random"));
+    // console.log(links.classList.contains("links"));
+    if(links.classList.contains("show-links")){
+        // if class of show links is there, we are removing 
+        links.classList.remove("show-links")
+    } else {
+        links.classList.add("show-links")
+        // if class is not there aka it is false, we are showing the links
+    }
+})
