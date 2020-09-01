a6120cce1cda3a99b79c63f03ac37839a29e3afe
ptran12 <phitran.722@gmail.com>
2020-09-01 17:15:16 -0500

    buttons dynamic gen

--- project_test8/app.js
+++ project_test8/app.js
@@ -71,15 +71,40 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
 
 const sectionCenter = document.querySelector('.section-center');
+const container = document.querySelector('.btn-container')
+
 const filterBtns = document.querySelectorAll('.filter-btn');
 
 window.addEventListener("DOMContentLoaded", function () {
   // console.log('test');
   displayMenuItems(menu)
+  const categories = menu.reduce(function(values, item){
+    if(!values.includes(item.category)){
+      values.push(item.category);
+    }
+    return values;
+  },
+  ["all"]
+  );
+  const categoryBtns = categories.map(function(category){
+    return `<button class="filter-btn" type="button" data-id=${category}>${category}</button>`
+  })
+  .join("") 
+  // console.log(categoryBtns);
+  container.innerHTML = categoryBtns
 });
+
 
 // filters
 filterBtns.forEach(function(btn){
