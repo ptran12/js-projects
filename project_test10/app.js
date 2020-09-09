8b662904d802eaa4e859e9871d205720fde6dc9d
ptran12 <phitran.722@gmail.com>
2020-09-09 15:45:37 -0500

    nav bar changes

--- project_test10/app.js
+++ project_test10/app.js
@@ -8,6 +8,23 @@ const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
 
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function(){
+    // linksContainer.classList.toggle('shows-links');
+    const containerHeight = linksContainer.getBoundingClientRect().height
+    const linksHeight = links.getBoundingClientRect().height
+    
+    if(containerHeight === 0){
+        linksContainer.style.height = `${linksHeight}px`
+    }
+    else{
+        linksContainer.style.height = 0;
+    }
+});
+
 
 // ********** fixed navbar ************
 
